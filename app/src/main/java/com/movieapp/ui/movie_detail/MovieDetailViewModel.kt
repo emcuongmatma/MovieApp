2dc3bc7b7aeb09a8aff66297884ab7326b10a3e3
emcuongmatma <nvc05042k4@gmail.com>
2025-05-04 16:06:01 +0700

    update

--- app/src/main/java/com/movieapp/ui/movie_detail/MovieDetailViewModel.kt
+++ app/src/main/java/com/movieapp/ui/movie_detail/MovieDetailViewModel.kt
@@ -1,5 +1,6 @@
 package com.movieapp.ui.movie_detail
 
+import android.util.Log
 import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -7,23 +8,31 @@ import androidx.media3.common.MediaItem
 import androidx.media3.common.MimeTypes
 import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
-import com.movieapp.domain.model.moviedetail.MovieDetailModel
-import com.movieapp.domain.model.moviedetail.MovieDetailResponseModel
-import com.movieapp.domain.model.nCModel.toMovieDetailResponseModel
-import com.movieapp.domain.repository.ApiRepository
+import com.movieapp.data.datasource.local.dao.MovieDao
+import com.movieapp.data.datasource.local.dao.ResumeMovieDetail
+import com.movieapp.data.model.moviedetail.MovieDetailModel
+import com.movieapp.data.model.moviedetail.MovieDetailResponseModel
+import com.movieapp.data.model.nCModel.toMovieDetailResponseModel
+import com.movieapp.data.repository.remote.ApiRepository
 import com.movieapp.ui.util.LoadStatus
-import com.movieapp.ui.util.MovieSourceManager
+import com.movieapp.data.datasource.remote.MovieSourceManager
+import com.movieapp.data.model.moviedetail.toCustomMovieModel
+import com.movieapp.ui.util.fixImg
 import com.skydoves.sandwich.ApiResponse
+import com.skydoves.sandwich.isSuccess
 import com.skydoves.sandwich.message
 import com.skydoves.sandwich.onError
 import com.skydoves.sandwich.onFailure
 import com.skydoves.sandwich.onSuccess
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import okhttp3.internal.wait
 import javax.inject.Inject
 
 
@@ -32,11 +41,13 @@ class MovieDetailViewModel
 @Inject constructor(
     private val apiRepository: ApiRepository,
     val player: ExoPlayer,
-    private val movieSourceManager: MovieSourceManager
+    private val movieSourceManager: MovieSourceManager,
+    private val movieDao: MovieDao
 ) : ViewModel() {
     private val _state = MutableStateFlow(MovieDetailState())
     val state = _state.asStateFlow()
     private lateinit var videoItems: MutableList<List<MediaItem>>
+
     init {
         player.prepare()
         player.playWhenReady = true
@@ -46,84 +57,141 @@ class MovieDetailViewModel
             }
         })
     }
+
     private fun loadListVideo() {
-        videoItems = _state.value.movie.episodes!!.map { servers ->
-            servers.serverData.map { ep ->
-                MediaItem.Builder().setUri(ep.linkM3u8).setTag(ep.name).setMediaId(ep.name!!)
-                    .setMimeType(MimeTypes.APPLICATION_M3U8).build()
-            }
-        }.toMutableStateList()
-        _state.value = _state.value.copy(
-            epSelected = videoItems[0].first().mediaId
-        )
-        playFirstEp(0)
-    }
-    private fun playFirstEp(server: Int) {
-        player.setMediaItem(videoItems[server][0])
-        player.play()
-    }
-    private fun playVideo(server: Int, ep: String) {
-        player.setMediaItem(videoItems[server].find { it.mediaId == ep }!!)
-        player.play()
-    }
-    fun getMovieDetail() {
-        _state.update { it.copy(status = LoadStatus.Loading()) }
-        viewModelScope.launch {
-             val result = if (movieSourceManager.currentSource.value is MovieSourceManager.MovieSource.NguonC) {
-                apiRepository.getMovieDetailNC(_state.value.slug)
-                    .onSuccess {
-                        _state.update {
-                            it.copy(
-                                status = LoadStatus.Success(),
-                                movie = data.toMovieDetailResponseModel(),
-                                serverSelected = 0
-                            )
-                        }
-                    }
+        _state.value.movie.episodes?.let {
+            videoItems = it.map { servers ->
+                servers.serverData.map { ep ->
+                    Log.e("123",servers.serverData.toString() + ep.name.toString())
+                    MediaItem.Builder().setUri(ep.linkM3u8).setTag(ep.name).setMediaId(ep.name!!)
+                        .setMimeType(MimeTypes.APPLICATION_M3U8).build()
+                }
+            }.toMutableStateList()
+            if (_state.value.resume.resume.isNotEmpty()) {
+                val svEp = _state.value.resume.resume.split(":")
+                _state.value = _state.value.copy(
+                    serverSelected = svEp[0].toInt(),
+                    epSelected = svEp[1]
+                )
             } else {
-                apiRepository.getMovieDetail(_state.value.slug)
-                    .onSuccess {
-                        _state.update {
-                            it.copy(
-                                status = LoadStatus.Success(),
-                                movie = data,
-                                serverSelected = 0
-                            )
-                        }
-                    }
+                _state.value = _state.value.copy(
+                    epSelected = videoItems[0].first().mediaId
+                )
             }
+            playVideo()
+        }
+    }
+
+    private fun playVideo() {
+        player.setMediaItem(videoItems[_state.value.serverSelected].find { it.mediaId == _state.value.epSelected }!!)
+        if (_state.value.resume.resumePositionMs > 0L)
+            player.seekTo(_state.value.resume.resumePositionMs)
+        player.play()
+    }
+
+    fun getMovieDetail() {
+        viewModelScope.launch {
+            _state.update { it.copy(status = LoadStatus.Loading()) }
+            movieDao.getResume(
+                _state.value.slug,
+                movieSourceManager.currentSource.value.index
+            )?.let {
+                _state.value = _state.value.copy(
+                    resume = it
+                )
+            }
+            movieDao.getFav(_state.value.slug)?.let {
+                _state.value = _state.value.copy(
+                    isFav = it
+                )
+            }
+            val result =
+                if (movieSourceManager.currentSource.value is MovieSourceManager.MovieSource.NguonC) {
+                    apiRepository.getMovieDetailNC(_state.value.slug)
+                        .onSuccess {
+                            _state.update {
+                                it.copy(
+                                    status = LoadStatus.Success(),
+                                    movie = data.toMovieDetailResponseModel(),
+                                    serverSelected = 0
+                                )
+                            }
+                        }
+                } else {
+                    apiRepository.getMovieDetail(_state.value.slug)
+                        .onSuccess {
+                            _state.update {
+                                it.copy(
+                                    status = LoadStatus.Success(),
+                                    movie = data,
+                                    serverSelected = 0
+                                )
+                            }
+                        }
+                }
             result.onError {
                 setToast(this.payload.toString())
-                }
+            }
                 .onFailure {
                     setToast(this.message())
                 }
-            if (result is ApiResponse.Success){
+            if (result.isSuccess){
                 delay(350) // for bottom bar animation
                 loadListVideo()
             }
         }
     }
+
+    fun addFavMovie() {
+        viewModelScope.launch {
+            movieDao.insert(
+                _state.value.movie.movie!!.toCustomMovieModel().copy(
+                    isFav = !_state.value.isFav,
+                    source = movieSourceManager.currentSource.value.index
+                ).fixImg(movieSourceManager)
+            )
+            _state.value = _state.value.copy(isFav = !_state.value.isFav)
+        }
+    }
+
     fun pausePlayer() {
         player.pause()
+        viewModelScope.launch {
+            if (!_state.value.movie.movie?.slug.isNullOrEmpty()) {
+                movieDao.insert(
+                    _state.value.movie.movie!!.toCustomMovieModel().copy(
+                        isResume = true,
+                        source = movieSourceManager.currentSource.value.index,
+                        isFav = _state.value.isFav,
+                        resume = "${_state.value.serverSelected}:${_state.value.epSelected}",
+                        resumePositionMs = player.currentPosition,
+                        durationMs = player.duration
+                    ).fixImg(movieSourceManager)
+                )
+            }
+        }
         player.clearMediaItems()
         _state.update {
             it.copy(
                 status = LoadStatus.Init(),
                 movie = MovieDetailResponseModel(null, MovieDetailModel()),
                 serverSelected = 0,
+                isFav = false,
+                resume = ResumeMovieDetail("", 0, 0),
                 slug = ""
             )
         }
     }
+
     fun onEpChange(name: String) {
         _state.update {
             it.copy(
                 epSelected = name
             )
         }
-        playVideo(_state.value.serverSelected, name)
+        playVideo()
     }
+
     fun onServerChange(int: Int) {
         _state.update {
             it.copy(
@@ -131,22 +199,25 @@ class MovieDetailViewModel
                 epSelected = videoItems[int].first().mediaId
             )
         }
-        playFirstEp(int)
+        playVideo()
     }
-    fun setToast(err:String){
+
+    fun setToast(err: String) {
         _state.update {
             it.copy(
                 status = LoadStatus.Error(err)
             )
         }
     }
-    fun reset(){
+
+    fun reset() {
         _state.update {
             it.copy(
                 status = LoadStatus.Init()
             )
         }
     }
+
     fun isFullScreen(boolean: Boolean) {
         _state.update {
             it.copy(
@@ -154,13 +225,15 @@ class MovieDetailViewModel
             )
         }
     }
-    fun setSlug(slug:String){
+
+    fun setSlug(slug: String) {
         _state.update {
             it.copy(
                 slug = slug
             )
         }
     }
+
     override fun onCleared() {
         pausePlayer()
         super.onCleared()
