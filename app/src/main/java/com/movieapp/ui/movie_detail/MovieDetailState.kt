f15742548c6c577a4ba05ee0d1a63212718f919d
emcuongmatma <nvc05042k4@gmail.com>
2025-05-04 16:05:24 +0700

    add support Fav screen

--- app/src/main/java/com/movieapp/ui/movie_detail/MovieDetailState.kt
+++ app/src/main/java/com/movieapp/ui/movie_detail/MovieDetailState.kt
@@ -1,6 +1,7 @@
 package com.movieapp.ui.movie_detail
 
-import com.movieapp.domain.model.moviedetail.MovieDetailResponseModel
+import com.movieapp.data.datasource.local.dao.ResumeMovieDetail
+import com.movieapp.data.model.moviedetail.MovieDetailResponseModel
 import com.movieapp.ui.util.LoadStatus
 
 data class MovieDetailState (
@@ -10,5 +11,7 @@ data class MovieDetailState (
     val status: LoadStatus = LoadStatus.Loading(),
     val slug:String="",
     val isFullScreen:Boolean = false,
-    val isPlaying: Boolean = false
+    val isPlaying: Boolean = false,
+    val resume: ResumeMovieDetail = ResumeMovieDetail("",0,0),
+    val isFav: Boolean = false
 )
